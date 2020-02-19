c22b43aef97dd3a232a3d71c7c3ac833765cce5c
Felipe <cvfelipeoliveira@gmail.com>
2020-02-18 23:00:52 -0500

    coments

--- src/main/java/br/com/fazenda/service/AnimalServiceImpl.java
+++ src/main/java/br/com/fazenda/service/AnimalServiceImpl.java
@@ -35,12 +35,18 @@ public class AnimalServiceImpl implements AnimalService {
 		this.animalRepository = animalRepository;
 	}
 
+	/**
+	 * adicionar novo animal no banco de dados
+	 */
 	@Override
 	public void adicionar(Double peso, Long idTipoAnimal) {
 		Animal animal = Animal.builder().peso(peso).tipoAnimal(TipoAnimal.builder().id(idTipoAnimal).build()).build(); 
 		animalRepository.save(animal);
 	}
 	
+	/**
+	 * listar os animais no banco de dados e calcular o peso medio por tipo de animal
+	 */
 	@Override
 	public List<PesoMedioDto> calcularPesoMedio() {
 		
@@ -53,6 +59,9 @@ public class AnimalServiceImpl implements AnimalService {
 				PesoMedioDto.builder().tipoAnimal("Galinhas").peso(mediaGalinhas).build());
 	}
 
+	/**
+	 * consultar a quantidade de animais a venda no banco de dados considerando o peso de venda
+	 */
 	@Override
 	public List<QuantidadeVendaDto> listarAnimaisVenda() {
 		Integer qtdVacas = quantidadeVendaPorTipoAnimal(1L);
@@ -64,6 +73,9 @@ public class AnimalServiceImpl implements AnimalService {
 				QuantidadeVendaDto.builder().tipoAnimal("Galinhas").quantidade(qtdGalinhas).build());
 	}
 	
+	/**
+	 * calcular o valor total da fazenda considerando a quantidade de animais no banco de dados e o valor de venda passado por parametro
+	 */
 	@Override
 	public Double consultarValorFazendaPorParametro(Double vaca, Double porco, Double galinha) {
 		Integer qtdVacas = quantidadePorTipoAnimal(1L);
@@ -73,7 +85,31 @@ public class AnimalServiceImpl implements AnimalService {
 		return format((qtdVacas * vaca) + (qtdPorcos * porco) + (qtdGalinhas * galinha));
 		
 	}
+	
+	/**
+	 * consultar o valor total do estoque no banco de dados considerando o valor e peso de venda de cada tipo de animal
+	 */
+	@Override
+	public Double consultarValorEstoque() {
+		List<Animal> listaAnimalPorTipo = animalRepository.findAll().stream().filter(animal -> animal.getPeso() >= animal.getTipoAnimal().getPesoVenda()).collect(Collectors.toList());
+		
+		Double valorEstoque = 0D;
+		if(!listaAnimalPorTipo.isEmpty()) {
+			for(Animal animal : listaAnimalPorTipo) {
+				valorEstoque += animal.getTipoAnimal().getValorMercado();
+			}
+			
+			return format(valorEstoque);
+		}
+		
+		return 0D;
+	}
 
+	/**
+	 * metodo acessorio para apoio no calculo de peso medio de cada tipo de animal
+	 * @param idTipoAnimal
+	 * @return
+	 */
 	private Double calcularPesoMedioPorTipoAnimal(Long idTipoAnimal) {
 		List<Animal> listaAnimalPorTipo = animalRepository.findAll().stream().filter(animal -> animal.getTipoAnimal().getId() == idTipoAnimal).collect(Collectors.toList());
 		Double peso = 0D;
@@ -91,37 +127,34 @@ public class AnimalServiceImpl implements AnimalService {
 		return 0D;
 	}
 	
+	/**
+	 * metodo acessorio para calcular a quantidade de animais por tipo
+	 * @param idTipoAnimal
+	 * @return
+	 */
 	private Integer quantidadePorTipoAnimal(Long idTipoAnimal) {
 		List<Animal> listaAnimalPorTipo = animalRepository.findAll().stream().filter(animal -> animal.getTipoAnimal().getId() == idTipoAnimal).collect(Collectors.toList());
 		return listaAnimalPorTipo.size();
 	}
 	
+	/**
+	 * metodo acessorio para calcular a quantidade de animais a venda por tipo
+	 * @param idTipoAnimal
+	 * @return
+	 */
 	private Integer quantidadeVendaPorTipoAnimal(Long idTipoAnimal) {
 		List<Animal> listaAnimalPorTipo = animalRepository.findAll().stream().filter(animal -> animal.getTipoAnimal().getId() == idTipoAnimal && animal.getPeso() >= animal.getTipoAnimal().getPesoVenda()).collect(Collectors.toList());
 		return listaAnimalPorTipo.size();
 	}
 	
+	/**
+	 * formatar o valor em duas casas decimais para exibicao
+	 * @param valor
+	 * @return
+	 */
 	private Double format(Double valor) {
 		DecimalFormat df = new DecimalFormat("#.##");      
 		return Double.valueOf(df.format(valor));
 	}
-
-	@Override
-	public Double consultarValorEstoque() {
-		List<Animal> listaAnimalPorTipo = animalRepository.findAll().stream().filter(animal -> animal.getPeso() >= animal.getTipoAnimal().getPesoVenda()).collect(Collectors.toList());
-		
-		Double valorEstoque = 0D;
-		if(!listaAnimalPorTipo.isEmpty()) {
-			for(Animal animal : listaAnimalPorTipo) {
-				valorEstoque += animal.getTipoAnimal().getValorMercado();
-			}
-			
-			return format(valorEstoque);
-		}
-		
-		return 0D;
-	}
-
-
 
 }
